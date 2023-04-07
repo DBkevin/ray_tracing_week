571973134850201baa6f9f350e9e0b489ba55816
admy <416606903@qq.com>
2023-04-07 18:04:22 +0800

    修改Vec3的类，添加上运算符重载

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use crate::vec3::*;
+use crate::{ray::Ray, vec3::Vec3};
+mod ray;
 mod vec3;
 
 fn main() {
@@ -8,14 +9,19 @@ fn main() {
     println!("P3");
     println!("{} {}", image_width, image_height);
     println!("255");
+
     for j in (0..image_height).rev() {
         eprintln!("Scanlines remaining:{}", j);
-
         for i in 0..image_width {
+            // let u=i as f64 /image_width as f64;
+            // let v=j as f64 / image_height as f64;
+            // let r =Ray::ray_color(origin, lower_left_coner+u*horizontal+v*vertical);
             let r: f64 = i as f64 / image_width as f64;
             let g: f64 = j as f64 / image_height as f64;
             let b = 0.2;
-            Vec3::new(r, g, b).write_color();
+            let color = Vec3::new(r, g, b);
+            color.get_color_string();
+            // Vec3::new(r, g, b).write_color();
             // let ir: i32 = (255.999 * r) as i32;
             // let ig: i32 = (255.999 * g) as i32;
             // let ib: i32 = (255.999 * b) as i32;
---
+++ src/ray.rs
@@ -0,0 +1,10 @@
+use crate::vec3::Vec3;
+
+pub struct  Ray{
+
+}
+impl Ray {
+	pub fn ray_color(origin:Vec3,direction:Vec3){
+
+	}
+}
